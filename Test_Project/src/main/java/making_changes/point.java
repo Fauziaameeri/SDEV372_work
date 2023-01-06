0d804b2b101bd77237dd446d97c23bc739b38ae9
fauziaameeri <ameeri.fauzia@student.greenriver.edu>
2023-01-06 14:24:23 -0800

    "Added a Point class to represent (x, y) coordinates."

---
+++ Test_Project/src/main/java/making_changes/point.java
@@ -0,0 +1,11 @@
+package making_changes;
+
+public class point {
+    private double x;
+    private double y;
+
+    public point(double x, double y){
+        this.x = y;
+        this.y = y;
+    }
+}
